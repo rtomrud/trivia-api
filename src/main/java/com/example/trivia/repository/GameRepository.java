dc4002c44b0abd4d54ce7c1420b1acb59d7218c4
Artem Rudenko <rtomrud@gmail.com>
2025-05-29 07:02:09 +0200

    Fix formatting of imports

--- src/main/java/com/example/trivia/repository/GameRepository.java
+++ src/main/java/com/example/trivia/repository/GameRepository.java
@@ -1,6 +1,7 @@
 package com.example.trivia.repository;
 
 import com.example.trivia.model.Game;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
